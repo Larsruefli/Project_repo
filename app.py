488873a583ea1263f9072866452c04834d354fbe
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-06 11:23:35 +0100

    Add Second Feature

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 import streamlit as st
 from Feature_01 import return_even
+from Feature_02 import return_odd
 
 original_list = [i for i in range(10)]
 
 even_list = return_even(original_list)
 
-
+odd_list = return_odd
 st.write("we connected everything")
 
 st.header("My header")
 
 st.write(even_list)
+
+st.write(odd_list)
+
