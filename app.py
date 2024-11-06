1b3720263e839dcbedd9a6903860943bb5791c17
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-06 11:19:24 +0100

    push to streamlit

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
+from Feature_01 import return_even
+
+original_list = [i for i in range(10)]
+
+even_list = return_even(original_list)
+
 
 st.write("we connected everything")
 
 st.header("My header")
 
+st.write(even_list)
