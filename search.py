9242da5b3be2ed491357c2e94460461d301c78a7
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 21:15:03 +0100

    d

--- search.py
+++ search.py
@@ -4,10 +4,10 @@ def test():
     st.header("Search Engine")
 
 
-    col1, col2 = st.columns([3,1])
+    col1, col2 = st.columns([2,1])
 
     with col1:
-        st.text_input("Geben Sie ein, wonach Sie suchen möchten:", placeholder="Type something...")
+        query = st.text_input("Geben Sie ein, wonach Sie suchen möchten:", placeholder="Type something...")
     with col2:
         if st.button("Suchen"):
             st.write(f"Suchergebnisse für: {query}")
