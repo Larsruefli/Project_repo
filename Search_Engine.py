d4370efbd9417d3a8c934a0dfdb9fc34b81e5317
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 19:06:13 +0100

    fd

--- Search_Engine.py
+++ Search_Engine.py
@@ -1,12 +1,2 @@
 import streamlit as st
 
-def test():
-    # Titel der Web-App
-    st.title("Who am I?")
-
-    # Sidebar mit Navigationsoptionen
-    st.sidebar.title("Navigation")
-    option = st.sidebar.radio(
-        "Wählen Sie eine Option:",
-        ("Start", "Suchmaschine", "Spiel")
-    )
