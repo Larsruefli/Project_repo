2c09d14ca3dce2c088de29b350bcec6a2846863b
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 18:42:48 +0100

    b

--- unnötig/Feature_01.py
+++ unnötig/Feature_01.py
@@ -1,3 +1,20 @@
 def return_even(x): 
     return [i for i in x if i%2 ==0]
 
+
+#gehört in ein neues Dokument, welches mit Streamlit verbunden ist
+from Feature_01 import return_even
+from Feature_02 import return_odd
+
+original_list = [i for i in range(10)]
+
+even_list = return_even(original_list)
+
+odd_list = return_odd(original_list)
+
+st.write("we connected everything")
+
+st.write(even_list)
+
+st.write(odd_list)
+
