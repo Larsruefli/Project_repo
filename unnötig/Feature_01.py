8b480f8e9f336bbbdf1a4726b9cb4d7462eed8b5
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 18:43:36 +0100

    m

--- unnötig/Feature_01.py
+++ unnötig/Feature_01.py
@@ -1,6 +1,6 @@
 def return_even(x): 
     return [i for i in x if i%2 ==0]
-
+import streamlit as st
 
 #gehört in ein neues Dokument, welches mit Streamlit verbunden ist
 from Feature_01 import return_even
