96540ca29f33a98b90fa8a24a3824abe55c3bf53
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 21:08:36 +0100

    s

--- search.py
+++ search.py
@@ -6,8 +6,7 @@ def test():
     col1, col2 = st.columns([3,1])
 
     with col1:
-        query = st.text_input("Geben Sie ein, wonach Sie suchen möchten:", placeholder="Type something")
-    
+        query = st.text_input("Geben Sie ein, wonach Sie suchen möchten:", placeholder="Type something...")
     with col2:
         if st.button("Suchen"):
             st.write(f"Suchergebnisse für: {query}")
