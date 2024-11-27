b7ba0ea67f7204fd2fd885767950eeb2b1b1ce27
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-27 20:41:11 +0100

    s

--- search.py
+++ search.py
@@ -3,6 +3,6 @@ import streamlit as st
 def test():
     st.header("Suchmaschine")
     st.write("Hier könnte eine Suchmaschine implementiert werden.")
-    query = st.text_input("Geben Sie ein, wonach Sie suchen möchten:")
-    if st.button("Suchen"):
+    query = st.text_input("Geben Sie ein, wonach Sie suchen möchten:", key="query_input")
+    if st.button("Suchen", key="search_button"):
         st.write(f"Suchergebnisse für: {query}")
