74deb50d96897c8d6e03e2a98833c70fab257718
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-28 12:04:15 +0100

    logo

--- game.py
+++ game.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 def test():
-    st.header("das ist das Spiel")
+    st.header("Who am I?")
+
+    # Display the logo at the top of the page
+    st.image("logo.png", use_column_width=False, width=200)  # Adjust width as needed
+
     
