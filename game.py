efa457b63a8ef1f2149beaeb7a6d4d9f16578a51
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-30 00:30:17 +0100

    f

--- game.py
+++ game.py
@@ -4,6 +4,15 @@ def test():
     st.header("Who am I?")
 
     # Display the logo at the top of the page
-    
+
+
+    option = st.selectbox(
+    "How difficult should be the Game?",
+    ("None", "Easy", "Medium", "Hard"),
+    index=None,
+    placeholder="Select a difficulty level...",
+    )
+
+    st.write("You selected:", option)
 
     
