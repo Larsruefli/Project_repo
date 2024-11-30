a6a65ffbea3b2157b811033a181cefeb799aad68
Larsruefli <lars.ruefli@student.unisg.ch>
2024-11-30 16:00:34 +0100

    x

--- game.py
+++ game.py
@@ -32,13 +32,13 @@ def test():
     with col1:
         question_template = st.selectbox(
             "Choose a question:",
-            ["I am currently playing for ...(club)", "I play in ... (league)", "I am from ...(nationality)", "I used to play for ...(club)", "I am a ... winner (achievement)", "I am older than ...(age)", "I am younger than ...(age)", "I play as ...(position)", "I wear the shirt number ... at my current club (shirt number)", "I am taller than ...(height)", "I am shorter than ...(height)"]
+            ["I am currently playing for ...", "I play in ...", "I am from ...", "I used to play for ...", "I am a ... winner", "I am older than ...", "I am younger than ...", "I play as ...", "I wear the shirt number ... at my current club", "I am taller than ...", "I am shorter than ..."]
         )
 
     # Weiterführende Auswahl von Kriterien durch if Funktion
     with col2:
         selected = None
-        if question_template == "I am currently playing for ...(club)":
+        if question_template == "I am currently playing for ...":
             current_club = st.selectbox(
                 "Choose my club:",
                 ["Real Madrid", "Arsenal", "Liverpool"] #!!!!!
@@ -46,14 +46,14 @@ def test():
             selected = current_club
             
         
-        elif question_template == "I play in ... (league)":
+        elif question_template == "I play in ...":
             league = st.selectbox(
                 "Choose my league:",
                 ["Premier League", "Bundesliga", "Eredivise", "LaLiga", "Serie A", "Ligue 1", "Liga Portugal", "Süper Lig", "Jupiler Pro League"]
             )
             selected = league
 
-        elif question_template == "I am from ...(nationality)":
+        elif question_template == "I am from ...":
             nationality = st.selectbox(
                 "Choose my country:",
                 ["Germany", "Switzerland", "Spain"]
@@ -61,54 +61,54 @@ def test():
             selected = nationality
 
 
-        elif question_template == "I used to play for ...(club)":
+        elif question_template == "I used to play for ...":
             past_club = st.selectbox(
                 "Choose a club:",
                 ["Real Mardid", "Arsenal", "Liverpool"]
             )
             selected = past_club
 
-        elif question_template == "I am a ... winner (achievement)":
+        elif question_template == "I am a ... winner":
             achievements = st.selectbox(
                 "Choose my achievements:",
                 ["Top 5 league", "Champions League", "World Cup", "European championship", "Europa League"]
             )
             selected = achievements
 
-        elif question_template == "I am older than ...(age)":
+        elif question_template == "I am older than ...":
             age = st.slider("",
                 min_value=15, max_value=45, value=30, step=1
             )
             selected = f"older than {age}"
 
-        elif question_template == "I am younger than ...(age)":
+        elif question_template == "I am younger than ...":
             age = st.slider("",
                 min_value=15, max_value=45, value=30, step=1
             )
             selected = f"younger than {age}"
 
-        elif question_template == "I play as ...(position)":
+        elif question_template == "I play as ...":
             position = st.selectbox(
                 "Choose a position:",
                 ["GK", "Defender", "Midfielder", "Striker"]
             )
             selected = position
 
-        elif question_template == "I wear the shirt number ... at my current club (shirt number)":
+        elif question_template == "I wear the shirt number ... at my current club":
             shirt_number = st.selectbox(
                 "Write a shirt number:",
                 ["Unter 20", "20-30", "Über 30"]
             )
             selected = shirt_number
 
-        elif question_template == "I am taller than ...(height)":
+        elif question_template == "I am taller than ...":
             height = st.slider(
                 "Select my height:",
                 min_value=150, max_value=220, value=185, step=1
             )
             selected = f"taller than {height} cm"
 
-        elif question_template == "I am shorter than ...(height)":
+        elif question_template == "I am shorter than ...":
             height = st.slider(
                 "Select my height:",
                 min_value=150, max_value=220, value=185, step=1
