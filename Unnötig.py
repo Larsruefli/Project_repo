37747d8fb5ec05511a06fbddacafb4f4cec54369
Larsruefli <lars.ruefli@student.unisg.ch>
2024-12-02 15:04:19 +0100

    dd

--- Unnötig.py
+++ Unnötig.py
@@ -17,25 +17,3 @@ with col3:
         status = f'<span style="color:green;">Yes</span>' if is_correct else f'<span style="color:red;">No</span>'
         st.markdown(f"{i}. {question} - {status}", unsafe_allow_html=True)
 
-
-   
-
-
-# Two Columns for the Gussing Input of the User and the Button Guess
-    col1, col2 = st.columns([3,1], vertical_alignment="bottom")
-
-    with col1: 
-        user_input = st.text_input("Enter your input here:", label_visibility="collapsed", placeholder="Type Player here")
-
-    with col2:
-        button_clicked = st.button("Guess")
-# When Button clicked it shows either a success Input with a picture of the Player or an error message
-        if button_clicked:
-            if user_input in players_data:
-            # Spieler gefunden
-                st.success("🎉 You guessed the player correctly!")
-            # Zeige das Bild in der Mitte
-                st.image(players_data[user_input], caption=f"{user_input}", width=200)
-            else:
-            # Spieler nicht gefunden
-                st.error("❌ That's the wrong player. Try again!")
